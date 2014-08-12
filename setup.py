abcf552ae026a1e68369859b9ba9fc281b40c039
Eduardo Erlo <eduardo.erlo@gmail.com>
2014-08-12 13:39:41 -0300

    Setup requires updated;

--- setup.py
+++ setup.py
@@ -12,5 +12,7 @@ setup(
     description='Python class to get real-time currency values, based on the information providing backends.',
     long_description=open('README.md').read(),
     install_requires=[
+        "cssselect==0.9.1",
+        "lxml==3.3.5"
     ],
 )
